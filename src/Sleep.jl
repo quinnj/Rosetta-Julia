4585f1213c0e3fe98498507233ff70eb7f552758
Leah Hanson <astrieanna@gmail.com>
2013-05-07 14:51:21 -0400

    added implementation of Sleep task.

---
+++ src/Sleep.jl
@@ -0,0 +1,9 @@
+# Sleep
+# http://rosettacode.org/wiki/Sleep
+
+print("Please enter sleep duration in seconds: ")
+input = int(readline(STDIN))
+println("Sleeping...")
+sleep(input)
+println("Awake!")
+
