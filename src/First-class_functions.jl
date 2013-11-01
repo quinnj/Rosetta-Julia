4b5c4a7ec31d3fd51cdaeff682b443839b165b06
Ben Racine <ben.racine@cornerstonenw.com>
2013-10-31 22:42:39 -0700

    give hte first class functions some assertions

--- src/First-class_functions.jl
+++ src/First-class_functions.jl
@@ -5,18 +5,25 @@ cube(x) = x^3
 cuberoot(x) = x^(1/3)
 
 # Higher order function
-function compose(f, g) 
-  	return (x) -> f(g(x));
+function compose(tuple)
+    (f, g) = tuple
+    return (x) -> f(g(x));
 end
  
 # Storing functions in a array
 fun = [sin, cos, cube]
 inv = [asin, acos, cuberoot]
  
+# Print out the imperative solution
+result = []
 for i = 1:3
-    println(compose(inv[i], fun[i])(0.5))
+    result = [result, compose(inv[i], fun[i])(0.5)]
 end
+@assert sum(result - [0.5, 0.5, 0.5]) < 0.01
 
-# 0.5
-# 0.4999999999999999
-# 0.5
+# A more functional approach
+fns = [sin, cos, x -> x^3]
+inv = [asin, acos, x -> x ^(1/3)]
+identities = map(compose, collect(zip(fns, inv)))
+result = map(x -> x(0.5), identities)
+@assert sum(result - [0.5, 0.5, 0.5]) < 0.01
